72fbe16e27cae38d426f03ee22fcf42ed4200263
FarlinNurjananti <farlinnurjananti@gmail.com>
2021-11-14 10:10:47 +0700

    first commit

---
+++ Farlin Nurjananti_212410102041.py
@@ -0,0 +1,15 @@
+nama_saya = 'Farlin Nurjananti' # String karena berisikan karakter/kalimat yang berupa huruf
+nomor_NIM = 212410102041 # Integer karena berisikan bilangan bulat
+umur_saya = 18 # Integer karena berisikan bilangan bulat
+tinggiBadan_saya = 150 # Integer karena berisikan bilangan bulat
+beratBadan_saya = 40.1 # Float karena berisikan bilangan berkoma
+statusKejombloan = True # Boolean karena hanya bernilai True or False
+
+print("nama          :", nama_saya)
+print("NIM           :", nomor_NIM)
+print("umur          :", umur_saya)
+print("Tinggi Badan  :", tinggiBadan_saya)
+print("Berat Badan   :", beratBadan_saya)
+print("Status Jomblo :", statusKejombloan)
+
+
